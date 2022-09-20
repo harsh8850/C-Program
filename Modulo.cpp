5ceb35bc890856af1cfaa646a4e409fcebbd4c5b
harsh8850 <113601815+harsh8850@users.noreply.github.com>
2022-09-20 18:57:21 +0530

    Modulus in c++

---
+++ Modulo.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std ;
+int main (){
+    int num1,num2 ;
+    cout<< "Enter the num1 \n" ;
+    cin>> num1;
+    cout<< "Enter the num2 \n" ;
+    cin>> num2;
+    cout<<"The value of num1 and num2 is :"<<num1%num2 ;
+    return 0;
+    
+}
