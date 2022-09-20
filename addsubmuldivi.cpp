bc52222bd76fa371e83c9a3f6d95e863c469053f
harsh8850 <113601815+harsh8850@users.noreply.github.com>
2022-09-20 18:32:58 +0530

    Addition ,Subtraction , Multiplication, Division program in C++

---
+++ addsubmuldivi.cpp
@@ -0,0 +1,55 @@
+//Sum of Two number in c++
+#include<iostream>
+using  namespace std ;
+int main (){
+    float num1 , num2;
+    cout<< "Enter the value of num1 \n";
+    cin>>num1;
+    cout<<"Enter the value of num2 \n" ;
+    cin>>num2;
+    cout<<"the value  of num1 and num2 is "<<num1+num2;
+    
+    return 0;
+}
+/*Subtraction of Two number in c++
+#include<iostream>
+using  namespace std ;
+int main (){
+    float num1 , num2;
+    cout<< "Enter the value of num1 \n";
+    cin>>num1;
+    cout<<"Enter the value of num2 \n" ;
+    cin>>num2;
+    cout<<"the value  of num1 and num2 is "<<num1-num2;
+    
+    return 0;
+}*\
+
+
+/*multiplication of Two number in c++
+#include<iostream>
+using  namespace std ;
+int main (){
+    float num1 , num2;
+    cout<< "Enter the value of num1 \n";
+    cin>>num1;
+    cout<<"Enter the value of num2 \n" ;
+    cin>>num2;
+    cout<<"the value  of num1 and num2 is "<<num1*num2;
+    
+    return 0;
+}*\
+
+/*Division of Two number in c++
+#include<iostream>
+using  namespace std ;
+int main (){
+    float num1 , num2;
+    cout<< "Enter the value of num1 \n";
+    cin>>num1;
+    cout<<"Enter the value of num2 \n" ;
+    cin>>num2;
+    cout<<"the value  of num1 and num2 is "<<num1/num2;
+    
+    return 0;
+}*\
