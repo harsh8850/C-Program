bf7b5243ee0c170cd3605e3650b82aa479154baf
harsh8850 <113601815+harsh8850@users.noreply.github.com>
2022-09-20 15:38:08 +0530

    Add files via upload

---
+++ first.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+int main (){
+    using namespace std;
+    int a=4;
+    int b=5;
+    float pi= 3.14 ;
+    cout<<"This is totorial 4 . \n Here The value of a is "<<a ; 
+
+    cout<<" \n The value of b is  "<< b ; 
+     
+     cout<< " \nThe value of pi is :"<<pi ;
+    return 0;
+}
