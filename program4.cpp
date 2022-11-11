35530e5b372a400891e70aa4a36655799bfb27a5
HARSH VERMA <113601815+harsh8850@users.noreply.github.com>
2022-11-11 10:39:19 +0530

    eligibility checking
    eligibility checking

---
+++ program4.cpp
@@ -0,0 +1,15 @@
+// eligibility checking
+#include<iostream>
+using namespace std;
+int main(){
+    int Percentage ;
+    cout<<"Enter the Percentage \n";
+    cin>>Percentage;
+    if (Percentage>30){
+            cout<<"you are eligible for next class \n";
+        }
+        else{
+            cout<<"you not are eligible for next class  \n";
+        }
+        return 0;
+}
