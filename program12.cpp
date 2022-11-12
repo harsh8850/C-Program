4e59921cb2029c48ac362dad18d5312a505fb2bd
HARSH VERMA <113601815+harsh8850@users.noreply.github.com>
2022-11-12 17:33:06 +0530

    //Display my name harsh verma  5 times....
    //Display my name harsh verma  5 times....

---
+++ program12.cpp
@@ -0,0 +1,10 @@
+//Display my name harsh verma  5 times....
+#include<iostream>
+using namespace std;
+int main (){
+    for(int i=1 ; i<=5;++i){
+        cout<<"harsh verma"<<endl;
+
+    }
+    return 0;
+}
