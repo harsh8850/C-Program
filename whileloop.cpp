89fb64c8925a3348b4cfdbf33da5dbf82436c52e
HARSH VERMA <113601815+harsh8850@users.noreply.github.com>
2022-11-13 21:21:30 +0530

    while loop
    while loop

---
+++ whileloop.cpp
@@ -0,0 +1,15 @@
+//use of while loop....
+#include<iostream>
+using  namespace std;
+int main (){
+    int n ;
+    cout<<"Enter the number \n";
+    cin>>n;
+    while(n>0){
+        cout<<n<<endl;
+        cin>>n;
+    }
+
+    return 0;
+
+}
