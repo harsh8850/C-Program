9fb170716b3941d49d0c9608ace74f9e3c354434
HARSH VERMA <113601815+harsh8850@users.noreply.github.com>
2022-11-13 21:27:01 +0530

    C++ program to illustrate while loop
    C++ program to illustrate while loop

---
+++ program15.cpp
@@ -0,0 +1,11 @@
+//// C++ program to illustrate while loop
+#include<iostream>
+using namespace std;
+int main(){
+    int i=1;
+    while(i>=-5){
+        cout<<i<<endl;
+        i--;
+    }
+    return 0;
+}
